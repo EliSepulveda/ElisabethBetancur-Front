52aa3348ed3aecba02b15f82561111ca8c0831df
EliSepulveda <elibetancur18@gmail.com>
2023-03-07 14:42:22 -0600

    Commit Examen

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Forms from './components/Form'
+
+function App() {
+  return (
+    <>
+      <Form/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+
+const CardCorrecta = ({nombre},{color}) => {
+  return (
+    <>
+        <h1>Felicidades</h1>
+        <h2>Haz ingresado {nombre} y tu color favorito es {color}</h2>
+    </>
+  )
+}
+
+export default CardCorrecta
